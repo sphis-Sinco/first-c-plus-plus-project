d280d0cf27d6d092907b0f4f584b216a069d1374
IdealistCat <mattallan.h@gmail.com>
2025-01-11 19:00:50 -0600

    i like burgers

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    // This is a comment
+    std:cout << "I like Burgers!" << '\n';
+    std:cout << "They are just so good. Y'know?'" << std:endl;
+    return 0;
+}
